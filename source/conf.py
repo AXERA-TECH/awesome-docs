9e553a8a01886fac3cf36734abc889d99deb22f4
jessenchen <41457236+jessenchen@users.noreply.github.com>
2026-08-03 19:47:22 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx_tabs.tabs',
     'sphinx_copybutton',       # 一键复制功能
     #'sphinxcontrib.svg2pdfconverter', #apt_packages内需要librsvg2-bin
-    'linkify',                 # 自动将纯文本 URL 转换为可点击的超链接
+    #'linkify',                 # 自动将纯文本 URL 转换为可点击的超链接
 ]
 
 templates_path = ['_templates']
