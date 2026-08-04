912cad7a46e068775bc0f2789de65c3f602ac882
jessenchen <41457236+jessenchen@users.noreply.github.com>
2026-08-04 10:55:09 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -33,12 +33,12 @@ extensions = [
     'myst_parser',
     "sphinx.ext.mathjax",      # 渲染数学公式的引擎，支持Latex风格
     'sphinx_copybutton',       #代码块一键复制功能，匹配requirement.txt安装的sphinx-copybutton
-    #'sphinxcontrib.mermaid',   #流程图/时序图支持，匹配sphinxcontrib-mermaid
-    #'sphinxcontrib.plantuml',  #plantuml支持，匹配sphinxcontrib-plantuml
-    #'breathe'
+    'sphinxcontrib.mermaid',   #流程图/时序图支持，匹配sphinxcontrib-mermaid
+    'sphinxcontrib.plantuml',  #plantuml支持，匹配sphinxcontrib-plantuml
+    'breathe'
     'sphinx_tabs.tabs',
-    'sphinxcontrib.svg2pdfconverter', #apt_packages内需要librsvg2-bin
-    #'linkify',                 # 自动将纯文本 URL 转换为可点击的超链接
+    #'sphinxcontrib.svg2pdfconverter', #apt_packages内需要librsvg2-bin
+    'linkify',                 # 自动将纯文本 URL 转换为可点击的超链接
 ]
 
 templates_path = ['_templates']
@@ -82,13 +82,13 @@ plantuml = 'plantuml'
 plantuml_output_format = 'svg'
 
 # -- myst_parser支持------------------------------------------------------------
-#myst_enable_extensions = [
-#    'colon_fence',        # 允许使用 ::: 作为代码围栏的定界符，替代传统的 ```
-#    'deflist',            # 启用定义列表语法，类似于 HTML 中的 <dl> 标签
-#    'linkify',            # 自动将文档中的裸 URL（如 https://example.com）转换为可点击的超链接
-#    'tasklist',           # 支持 GitHub 风格的任务列表语法，如 - [x] 已完成
+myst_enable_extensions = [
+    'colon_fence',        # 允许使用 ::: 作为代码围栏的定界符，替代传统的 ```
+    'deflist',            # 启用定义列表语法，类似于 HTML 中的 <dl> 标签
+    'linkify',            # 自动将文档中的裸 URL（如 https://example.com）转换为可点击的超链接
+    'tasklist',           # 支持 GitHub 风格的任务列表语法，如 - [x] 已完成
     # 'dollarmath',         # 启用 LaTeX 风格的美元符号 $ 作为数学公式的定界符,
-#]
+]
 # 为文档中的标题自动生成 HTML 锚点（ID），方便其他页面或链接直接引用，仅作用前三级标题
 myst_heading_anchors = 3
 # 让 ```mermaid 围栏同时在 GitHub 预览与 Sphinx 构建中渲染为流程图。
@@ -97,9 +97,9 @@ myst_fence_as_directive = ['mermaid']
 myst_update_mathjax = True
 
 # --Breathe 连接 Doxygen 配置 -------------------------------------------------
-# breathe_projects = {"X2000": os.path.join(os.path.dirname(__file__), "..", "doxygen", "xml")}
-# breathe_default_project = "X2000"
-# breathe_default_members = ('members', 'undoc-members')
+breathe_projects = {"X2000": os.path.join(os.path.dirname(__file__), "..", "doxygen", "xml")}
+breathe_default_project = "X2000"
+breathe_default_members = ('members', 'undoc-members')
 
 # ----pdf格式生成
 #svg2pdf_converter = 'cairosvg'
